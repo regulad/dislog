df973c8578e4dba2a0d4e4b94425434d493b2116
Parker Wahle <parkeredwardwahle2017@gmail.com>
2022-01-22 10:54:24 -0500

    Better async

--- src/dislog/handler.py
+++ src/dislog/handler.py
@@ -1,4 +1,7 @@
-import datetime
+from asyncio import get_running_loop, AbstractEventLoop
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from functools import partial
 from logging import LogRecord, Handler, NOTSET
 from typing import Optional, List
 
@@ -6,19 +9,25 @@ from requests import Session
 
 
 class DiscordWebhookHandler(Handler):
+    """A logging handler that allows you to send messages with Discord Webhooks."""
+
     def __init__(
             self,
             webhook: str,
             level: int = NOTSET,
             *,
             session: Optional[Session] = None,
-            format: bool = False,
+            can_format: bool = False,
+            can_run_async: bool = True,
+            executor: Optional[ThreadPoolExecutor] = None,
     ):
         super().__init__(level)
         self._webhook: str = webhook
         self._new_client: bool = session is None
         self._session: Session = session or Session()
-        self._format: bool = format
+        self._format: bool = can_format
+        self._can_run_async: bool = True
+        self._executor: Optional[ThreadPoolExecutor] = None
 
     def close(self) -> None:
         self._session.post(self._webhook, json={
@@ -37,19 +46,31 @@ class DiscordWebhookHandler(Handler):
         else:
             color: int = 0xFFFFFF
 
-        self._session.post(self._webhook, json={
+        partial_post: partial = partial(self._session.post, self._webhook, json={
             "embeds": [
                 {
                     "title": f"{record.levelname} on {record.threadName} ({record.thread})",
                     "color": color,
                     "description": f"```{self.format(record) if self._format else record.getMessage()}```",
-                    "timestamp": datetime.datetime.utcfromtimestamp(record.created).isoformat(),
+                    "timestamp": datetime.utcfromtimestamp(record.created).isoformat(),
                     "footer": {
                         "text": record.name
                     }
                 }
             ]
         })
+
+        try:
+            maybe_running_loop: Optional[AbstractEventLoop] = get_running_loop()
+        except RuntimeError:
+            maybe_running_loop: Optional[AbstractEventLoop] = None
+        except Exception:
+            raise
+
+        if self._can_run_async and maybe_running_loop is not None:
+            maybe_running_loop.run_in_executor(self._executor, partial_post)
+        else:
+            partial_post()
 
 
 __all__: List[str] = [
