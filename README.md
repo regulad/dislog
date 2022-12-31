921101c9c692f9be86d1aad7696a6caa38b7445d
Parker Wahle <regulad@regulad.xyz>
2022-12-30 23:03:14 -0500

    2.0.0

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
-# DiscordWebhookLogger
+# [dislog](https://pypi.org/project/dislog/)
 
-Provides an interface for using a discord webhook as a logger.
+###### Previously [`discord-webhook-logger`](https://pypi.org/project/discord-webhook-logger/)
+
+Provides an interface for using a Discord webhook as a logger.
+
+Designed to abstract away webhook-specific details, such as the JSON format, and provide a simple interface for logging messages.
 
 ## Example
 
+Using `dislog` in your projects is dead simple. It behaves like any other `logging.Handler`.
+
+For performance reasons, it even fires off a new thread for each log message, so you don't have to worry about blocking your main thread with costly HTTP requests.
+
 ```py
-import dislog
-import logging
+from dislog import DiscordWebhookHandler
+from logging import *
 
-logging.basicConfig(level=logging.ERROR, handlers=[dislog.DiscordWebhookHandler("url")])
+basicConfig(level=ERROR, handlers=[StreamHandler(), DiscordWebhookHandler("url", DEBUG)])
 
-logging.error("hi")
+error("hi")
+```
+
+It also works with asynchronous code, simply pass the `run_async` keyword argument. This is optional and makes it use the event loop instead of a thread pool.
+
+```py
+from dislog import DiscordWebhookHandler
+from logging import *
+from asyncio import run, sleep
+
+async def main():
+    basicConfig(level=ERROR, handlers=[StreamHandler(), DiscordWebhookHandler("url", run_async=True)])
+    
+    error("hi")
+    
+    await sleep(1)  # Give it some time to run!
+
+run(main())
 ```
 
 ![img.png](img.png)
--- src/dislog/handler.py
+++ src/dislog/handler.py
@@ -1,11 +1,40 @@
+"""
+    DiscordWebhookLogger (dislog) provides an interface for using a Discord webhook as a logger.
+    Copyright (C) 2022 Parker Wahle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from asyncio import get_running_loop, AbstractEventLoop
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from functools import partial
 from logging import LogRecord, Handler, NOTSET
-from typing import Optional, List
+from typing import List
 
-from requests import Session
+from aiohttp import ClientSession
+from discord import SyncWebhook, Webhook, Embed
+
+DEPENDENCY_LOGGERS: set[str] = {"aiohttp", "discord", "asyncio", "concurrent.futures", "websockets", "urllib3"}
+
+
+def filter_out_dependencies(record: LogRecord) -> int:
+    """
+    Filters out records from dependencies. This is needed to not have an infinite loop of logging.
+    """
+
+    should_filter: bool = any([record.name.startswith(dependency) for dependency in DEPENDENCY_LOGGERS])
+
+    return 0 if should_filter else 1
 
 
 class DiscordWebhookHandler(Handler):
@@ -13,28 +42,50 @@ class DiscordWebhookHandler(Handler):
 
     def __init__(
             self,
-            webhook: str,
+            webhook: str | SyncWebhook | Webhook,
             level: int = NOTSET,
             *,
-            session: Optional[Session] = None,
             can_format: bool = False,
-            can_run_async: bool = True,
-            executor: Optional[ThreadPoolExecutor] = None,
+            run_async: bool = False,
     ):
         super().__init__(level)
-        self._webhook: str = webhook
-        self._new_client: bool = session is None
-        self._session: Session = session or Session()
+        self.addFilter(filter_out_dependencies)
+
         self._format: bool = can_format
-        self._can_run_async: bool = True
-        self._executor: Optional[ThreadPoolExecutor] = None
+        self._async: bool = run_async
+        self._webhook: SyncWebhook | Webhook
+
+        self._aiohttp_session: ClientSession | None = None
+        self._thread_pool: ThreadPoolExecutor | None = None
+
+        if isinstance(webhook, str):
+            if self._async:
+                self._aiohttp_session = ClientSession()
+                self._webhook = Webhook.from_url(webhook, session=self._aiohttp_session)
+            else:
+                self._webhook = SyncWebhook.from_url(webhook)
+                self._thread_pool = ThreadPoolExecutor(thread_name_prefix=f"dislog-{self._webhook.id}-")
+
+    def _send(self, *args, **kwargs) -> None:
+        if self._async:
+            assert isinstance(self._webhook, Webhook) and self._aiohttp_session is not None
+
+            loop: AbstractEventLoop = get_running_loop()
+            loop.create_task(self._webhook.send(*args, **kwargs))
+            # note: this might not happen instantly. that's ok though, because we timestamp the message.
+        else:
+            assert isinstance(self._webhook, SyncWebhook) and self._thread_pool is not None
+
+            self._thread_pool.submit(self._webhook.send, *args, **kwargs)
 
     def close(self) -> None:
-        self._session.post(self._webhook, json={
-            "content": "**CLOSED**"
-        })
-        if self._new_client:
-            self._session.close()
+        if self._async:
+            assert self._aiohttp_session is not None
+            self._aiohttp_session.close()
+        else:
+            assert self._thread_pool is not None
+            self._thread_pool.shutdown()
+        super().close()
 
     def emit(self, record: LogRecord) -> None:
         if record.levelname == "CRITICAL" or record.levelname == "FATAL" or record.levelname == "ERROR":
@@ -46,31 +97,21 @@ class DiscordWebhookHandler(Handler):
         else:
             color: int = 0xFFFFFF
 
-        partial_post: partial = partial(self._session.post, self._webhook, json={
-            "embeds": [
-                {
-                    "title": f"{record.levelname} on {record.threadName} ({record.thread})",
-                    "color": color,
-                    "description": f"```{self.format(record) if self._format else record.getMessage()}```",
-                    "timestamp": datetime.utcfromtimestamp(record.created).isoformat(),
-                    "footer": {
-                        "text": record.name
-                    }
-                }
-            ]
-        })
+        embed_dict: dict = {
+            "title": f"{record.levelname} on {record.threadName} ({record.thread})",
+            "color": color,
+            "description": f"```{self.format(record) if self._format else record.getMessage()}```",
+            "timestamp": datetime.utcfromtimestamp(record.created).isoformat(),
+            "footer": {
+                "text": record.name
+            }
+        }
 
-        try:
-            maybe_running_loop: Optional[AbstractEventLoop] = get_running_loop()
-        except RuntimeError:
-            maybe_running_loop: Optional[AbstractEventLoop] = None
-        except Exception:
-            raise
+        # patching over some legacy code here...
 
-        if self._can_run_async and maybe_running_loop is not None:
-            maybe_running_loop.run_in_executor(self._executor, partial_post)
-        else:
-            partial_post()
+        embed: Embed = Embed.from_dict(embed_dict)
+
+        self._send(embeds=[embed])
 
 
 __all__: List[str] = [
